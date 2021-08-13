62fb797051f79740ffce1955b277ff6d8d016bfe
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-13 16:56:59 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import *
+import time
+import random
+
+
+class Game:
+    def __init__(self):
+        self.w = 750
+        self.h = 500
+        self.reset = True
+        self.active = False
+        self.input_text = ''
+        self.word = ''
+        self.time_start = 0
+        self.total_time = 0
+        self.accuracy = '0%'
+        self.results = 'Time:0 Accuracy:0 % Wpm:0 '
+        self.wpm = 0
+        self.end = False
+        self.HEAD_C = (255, 213, 102)
+        self.TEXT_C = (240, 240, 240)
+        self.RESULT_C = (255, 70, 70)
+
+        pygame.init()
+        self.open_img = pygame.image.load('images/icon.png')
+        self.open_img = pygame.transform.scale(self.open_img, (self.w, self.h))
+
+        self.bg = pygame.image.load('images/background.jpg')
+        self.bg = pygame.transform.scale(self.bg, (750, 500))
+
+        self.screen = pygame.display.set_mode((self.w, self.h))
+
+    def draw_text(self, screen, msg, y, fsize, color):
+        font = pygame.font.Font(None, fsize)
+        text = font.render(msg, 1, color)
+        text_rect = text.get_rect(center=(self.w/2, y))
+        screen.blit(text, text_rect)
+        pygame.display.update()
+
