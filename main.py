6a76a66abc44f67be6339a3ef04438030fef2d7d
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-14 17:20:23 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pyjokes
 from pygame.locals import *
 import time
 import random
@@ -34,7 +35,32 @@ class Game:
     def draw_text(self, screen, msg, y, fsize, color):
         font = pygame.font.Font(None, fsize)
         text = font.render(msg, 1, color)
-        text_rect = text.get_rect(center=(self.w/2, y))
+        text_rect = text.get_rect(center=(self.w / 2, y))
         screen.blit(text, text_rect)
         pygame.display.update()
 
+    def get_sentence(self):
+        sentence = pyjokes.get_joke()
+        if len(sentence) <= 75:
+            return sentence
+
+    def show_results(self, screen):
+        if not self.end:
+
+            # Calculate time
+            self.total_time = time.time() - self.time_start
+
+            # Calculate accuracy
+            count = 0
+            for i, c in enumerate(self.word):
+                try:
+                    if self.input_text[i] == c:
+                        count += 1
+                except:
+                    pass
+            self.accuracy = count / len(self.word) * 100
+
+            # Calculate words per minute
+            self.wpm = len(self.input_text) * 60 / (5 * self.total_time)
+            self.end = True
+            print(self.total_time)
