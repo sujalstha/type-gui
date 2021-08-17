741f35c760ef45839d1926dd113f7dc4ac2837f4
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-16 22:37:14 -0500

    Initial commit

--- main.py
+++ main.py
@@ -78,3 +78,52 @@ class Game:
 
             # update the text of user input
             self.draw_text(self.screen, self.input_text, 274, 26, (250, 250, 250))
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    x, y = pygame.mouse.get_pos()
+                    # position of input box
+                    if (x >= 50 and x <= 650 and y >= 250 and y <= 300):
+                        self.active = True
+                        self.input_text = ''
+                        self.time_start = time.time()
+                    # position of reset box
+                    if (x >= 310 and x <= 510 and y >= 390 and self.end):
+                        self.reset_game()
+                        x, y = pygame.mouse.get_pos()
+                elif event.type == pygame.KEYDOWN:
+                    if self.active and not self.end:
+                        if event.key == pygame.K_RETURN:
+                            print(self.input_text)
+                            self.show_results(self.screen)
+                            print(self.results)
+                            self.draw_text(
+                                self.screen, self.results, 350, 28, self.RESULT_C)
+                            self.end = True
+
+                        elif event.key == pygame.K_BACKSPACE:
+                            self.input_text = self.input_text[:-1]
+                        else:
+                            try:
+                                self.input_text += event.unicode
+                            except:
+                                pass
+
+            pygame.display.update()
+
+        clock.tick(60)
+
+        def reset_game(self):
+            self.screen.blit(self.open_img, (0, 0))
+            pygame.display.update()
+            time.sleep(1)
+            self.reset = False
+            self.end = False
+            self.input_text = ''
+            self.word = ''
+            self.time_start = 0
+            self.total_time = 0
+            self.wpm = 0
