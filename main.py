a73ce2b1e67ce332b5ec36bf87d5a0baf920bdf6
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-17 16:20:19 -0500

    Initial commit

--- main.py
+++ main.py
@@ -127,3 +127,44 @@ class Game:
             self.time_start = 0
             self.total_time = 0
             self.wpm = 0
+            self.end = False
+            self.HEAD_C = (255, 213, 102)
+            self.TEXT_C = (240, 240, 240)
+            self.RESULT_C = (255, 70, 70)
+
+            pygame.init()
+            self.open_img = pygame.image.load('images/type-speed-open.png')
+            self.open_img = pygame.transform.scale(self.open_img, (self.w, self.h))
+
+            self.bg = pygame.image.load('images/background.jpg')
+            self.bg = pygame.transform.scale(self.bg, (750, 500))
+
+            self.screen = pygame.display.set_mode((self.w, self.h))
+            pygame.display.set_caption('CheetaToise')
+
+        def draw_text(self, screen, msg, y, fsize, color):
+            font = pygame.font.Font(None, fsize)
+            text = font.render(msg, 1, color)
+            text_rect = text.get_rect(center=(self.w / 2, y))
+            screen.blit(text, text_rect)
+            pygame.display.update()
+
+        def get_sentence(self):
+            sentence = pyjokes.get_joke()
+            if len(sentence) <= 75:
+                return sentence
+
+        def show_results(self, screen):
+            if (not self.end):
+
+                # Calculate time
+                self.total_time = time.time() - self.time_start
+
+                # Calculate accuracy
+                count = 0
+                for i, c in enumerate(self.word):
+                    try:
+                        if self.input_text[i] == c:
+                            count += 1
+                    except:
+                        pass
