aa38f3a34e1db047b9ba1b70fea8dbfa4877c2f3
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-21 22:49:49 -0500

    Initial commit

--- main.py
+++ main.py
@@ -217,3 +217,24 @@ class Game:
                             if 310 <= x <= 510 and y >= 390 and self.end:
                                 self.reset_game()
                                 x, y = pygame.mouse.get_pos()
+                            elif event.type == pygame.KEYDOWN:
+
+                                if self.active and not self.end:
+
+                                    if event.key == pygame.K_RETURN:
+                                        print(self.input_text)
+                                        self.show_results(self.screen)
+                                        print(self.results)
+                                        self.draw_text(
+                                            self.screen, self.results, 350, 28, self.RESULT_C)
+                                        self.end = True
+
+                                    elif event.key == pygame.K_BACKSPACE:
+                                        self.input_text = self.input_text[:-1]
+                                    else:
+                                        try:
+                                            self.input_text += event.unicode
+                                        except:
+                                            pass
+
+                            pygame.display.update()
