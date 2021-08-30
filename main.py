4681ae4636a52deea08467f91584d500e831f0e7
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-29 22:49:43 -0500

    Initial commit

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Game:
         self.screen = pygame.display.set_mode((self.w, self.h))
 
     def draw_text(self, screen, msg, y, fsize, color):
-        font = pygame.font.Font(None, fsize )
+        font = pygame.font.Font(None, fsize)
         text = font.render(msg, 1, color)
         text_rect = text.get_rect(center=(self.w / 2, y))
         screen.blit(text, text_rect)
